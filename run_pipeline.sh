8d05bcb9ba7432664b38acfce6a762e6def5b463
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-03 02:11:58 +0000

    Add run_pipeline which will run all the parts and generate the data

---
+++ run_pipeline.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo hive -e "
+USE urbanecm;
+
+DROP TABLE IF EXISTS wmcz_outreach_dashboard_courses_csv;
+$(cat 00_Schema/wmcz_outreach_dashboard_courses_csv.hql)
+DROP TABLE IF EXISTS wmcz_outreach_dashboard_courses_users_csv;
+$(cat 00_Schema/wmcz_outreach_dashboard_courses_users_csv.hql)
+DROP TABLE IF EXISTS wmcz_outreach_dashboard_edits;
+$(cat 00_Schema/wmcz_outreach_dashboard_edits.hql)
+"
+
+bash 01_Import_dashboard_users/load_dashboard_courses.sh
+
+hive -f 02_Load_edits/load_data_to_edits.sql
+
+bash 03_Generate_dump/generate_dump.sh

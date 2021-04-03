4a865814f3841e6118c1af87f04da1e41a4f81a1
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-03 17:03:12 +0200

    Finalize run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -5,7 +5,8 @@ set -e
 scriptdir="`dirname \"$0\"`"
 cd $scriptdir
 
-echo hive -e "
+# STEP 1: Drop all tables and recreate them using DDL files in 00_Schema
+hive -e "
 USE urbanecm;
 
 DROP TABLE IF EXISTS wmcz_outreach_dashboard_courses_csv;
@@ -16,8 +17,13 @@ DROP TABLE IF EXISTS wmcz_outreach_dashboard_edits;
 $(cat 00_Schema/wmcz_outreach_dashboard_edits.hql)
 "
 
+# STEP 2: Refresh our knowledge about Outreach Dashboard courses
 bash 01_Import_dashboard_users/load_dashboard_courses.sh
 
+# STEP 3: Build table with edits
 hive -f 02_Load_edits/load_data_to_edits.sql
 
+# STEP 4: Generate data dump
+# For now, accessible from https://people.wikimedia.org/~urbanecm/wmcz/dashboard-stats-data/, but that's likely to change
+# when the pipeline becomes actually used by WMCZ
 bash 03_Generate_dump/generate_dump.sh

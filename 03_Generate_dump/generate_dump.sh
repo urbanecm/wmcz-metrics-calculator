926a15cfeab02bb7f8fbe13d8b83f7bef797a200
Martin Urbanec <martin.urbanec@wikimedia.cz>
2021-04-03 22:47:43 +0000

    generate_dump.sh: minor fixes

--- 03_Generate_dump/generate_dump.sh
+++ 03_Generate_dump/generate_dump.sh
@@ -16,12 +16,14 @@ cp ../00_Schema/wmcz_outreach_dashboard_edits.hql schema
 cp ../00_Schema/wmcz_outreach_dashboard_courses_users_csv.hql schema
 cp ../00_Schema/wmcz_outreach_dashboard_courses_csv.hql schema
 
-# Dump data and DESCRIBE output
-hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_edits" > schema/wmcz_outreach_dashboard_edits.txt
+# Copy txt version of schema (disabled, not necessary for now)
+#hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_edits" > schema/wmcz_outreach_dashboard_edits.txt
+#hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_courses_users_csv" > schema/wmcz_outreach_dashboard_courses_users_csv.txt
+#hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_courses_csv" > schema/wmcz_outreach_dashboard_courses_csv.txt
+
+# Dump data
 hive -e "SELECT * FROM urbanecm.wmcz_outreach_dashboard_edits" > wmcz_outreach_dashboard_edits.txt
-hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_courses_users_csv" > schema/wmcz_outreach_dashboard_courses_users_csv.txt
 hive -e "SELECT * FROM urbanecm.wmcz_outreach_dashboard_courses_users_csv" > wmcz_outreach_dashboard_courses_users_csv.txt
-hive -e "DESCRIBE urbanecm.wmcz_outreach_dashboard_courses_csv" > schema/wmcz_outreach_dashboard_courses_csv.txt
 hive -e "SELECT * FROM urbanecm.wmcz_outreach_dashboard_courses_csv" > wmcz_outreach_dashboard_courses_csv.txt
 
 # Create an archive
